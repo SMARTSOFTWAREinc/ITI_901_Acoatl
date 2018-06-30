96400842fce542f31eaa023bbc1714631eab0900
yisusantos <36114777+yisusantos@users.noreply.github.com>
2018-06-30 15:10:21 -0500

    creación de vistas del backend

---
+++ SitioWeb/AcoatlKayaks/application/views/admin/Template/footer_vw.php
@@ -0,0 +1,11 @@
+<footer><p>All right reserved. Template by: Acoatl Kayaks</p></footer>
+            </div>
+            <!-- /. PAGE INNER  -->
+        </div>
+        <!-- /. PAGE WRAPPER  -->
+    </div>
+ 
+
+</body>
+
+</html>
